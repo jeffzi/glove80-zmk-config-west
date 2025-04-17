1a4c981505087d35cf43b30cfe49ca3efe3b8546
Jean-Francois Zinque <16199817+jeffzi@users.noreply.github.com>
2025-04-17 12:14:15 +0200

    feat: add copy cut paste combos

--- config/constants.h
+++ config/constants.h
@@ -1,13 +1,29 @@
 #pragma once
 
+/* GLOVE80 KEY MATRIX
+ ╭────────────────────╮       ╭────────────────────╮
+ │  0   1   2   3   4 ╰───┬───╯  5   6   7   8   9 │
+ │ 10  11  12  13  14  15 │ 16  17  18  19  20  21 │
+ │ 22  23  24  25  26  27 │ 28  29  30  31  32  33 │
+ │ 34  35  36  37  38  39 │ 40  41  42  43  44  45 │
+ │ 46  47  48  49  50  51 │ 58  59  60  61  62  63 │
+ │ 64  65  66  67  68 ╭───┴───╮ 75  76  77  78  79 │
+ ╰───────────┬────────┴───┬───┴────────┬───────────╯
+             │ 52  53  54 │ 55  56  57 │
+             │ 69  70  71 │ 72  73  74 │
+             ╰────────────┴────────────╯
+ */
+
+#include "constants.h"
 // Keys
 #define ___ &trans
 #define XXX &none
-#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4
-#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4
-#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2
+#define KEYS_L 27 26 25 24 23 39 38 37 36 35 51 50 49 48 47
+#define KEYS_R 28 29 30 31 32 40 41 42 43 44 58 59 60 61 62
+#define THUMBS 69 70 71 72 73 74
 #define HYPR LC(LS(LG(LALT)))
 #define MEH LS(LC(LALT))
+#define HYP(key) LS(LC(LA(LG(key))))
 
 // layers
 #define BASE 0
